554e6242e36694098a44b95d9fd5baa890e3b802
Alexander Shevchuk <200454517@student.georgianc.on.ca>
2022-10-02 23:41:22 -0400

    added the required info to MainActivity file.

--- app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
@@ -3,6 +3,15 @@ package ca.georgiancollege.androidcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+* File name: MainActivity.kt
+* Author's name: Alexander Shevchuk
+* StudentID: 200454517
+* Date: October 2, 2022
+* App Description: Calculator built for android devices.
+* Version: 0.1
+* */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
