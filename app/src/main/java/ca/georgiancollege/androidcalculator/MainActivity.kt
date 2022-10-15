23dc0a80828b8b2dd483715a9d47754a81f40686
Alexander Shevchuk <200454517@student.georgianc.on.ca>
2022-10-15 17:17:05 -0400

    - Implemented the different shapes.
    - Started on development of the backend.

--- app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
@@ -2,6 +2,9 @@ package ca.georgiancollege.androidcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 /*
 * File name: MainActivity.kt
@@ -13,8 +16,38 @@ import android.os.Bundle
 * */
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // Instance members of the MainActivity class
+    var resultLabel: TextView? = null
+    var operationSymbols = arrayListOf<String>("÷", "×", "-", "+")
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultLabel = findViewById<TextView>(R.id.resultLabel);
+
+        resultLabel?.text = "0";
     }
+
+    fun ButtonPressed(view: View)
+    {
+        val pressedButtonInfo = view as Button
+        val pressedButtonText = pressedButtonInfo.text
+
+        when(pressedButtonText){
+            "0" -> resultLabel?.text = pressedButtonText;
+            else -> {
+//                resultLabel?.text += pressedButtonText;
+            }
+        }
+
+    }
+
+    fun Evaluate()
+    {
+
+    }
+
 }
