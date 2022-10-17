d5e3247b4ef45de267ddba5cfa9123518b4a218a
Alexander <LSharkie>
2022-10-16 21:51:21 -0400

    - Quite a few bug fixes which would case the program to break if a certain button was pressed during the equation. An example is pressing = when the last thing entered is an operator.
    - Added comments throughout the code.
    
    - Took care of all of the warnings.
    
    - At the moment, no bugs exist that I am aware of and the calculator is ready for use.

--- app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/androidcalculator/MainActivity.kt
@@ -1,13 +1,11 @@
 package ca.georgiancollege.androidcalculator
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.flow.combine
+import kotlin.math.roundToInt
 
 /*
 * File name: MainActivity.kt
@@ -15,28 +13,35 @@ import kotlinx.coroutines.flow.combine
 * StudentID: 200454517
 * Date: October 2, 2022
 * App Description: Calculator built for android devices.
-* Version: 0.1
+* Version: 1.0
 * */
 
 class MainActivity : AppCompatActivity() {
 
     // Instance members of the MainActivity class
-    var resultLabel: TextView? = null
-    var historyLabel: TextView? = null
-    var operationSymbols = arrayListOf<String>("÷", "×", "-", "+", "%")
+    private var resultLabel: TextView? = null
+    private var historyLabel: TextView? = null
+    private var operationSymbols = arrayListOf("÷", "×", "-", "+", "%")
 
+
+    /*
+    * Default on create function which generates the view.
+    */
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultLabel = findViewById<TextView>(R.id.resultLabel);
-        historyLabel = findViewById<TextView>(R.id.historyLabel);
+        resultLabel = findViewById(R.id.resultLabel)
+        historyLabel = findViewById(R.id.historyLabel)
 
-        resultLabel?.text = "0";
+        resultLabel?.text = "0"
     }
 
-    fun ButtonPressed(view: View)
+    /*
+    * This method is responsible for all of the operator and number buttons.
+    */
+    fun buttonPressed(view: View)
     {
         val pressedButtonInfo = view as Button
         val pressedButtonText = pressedButtonInfo.text
@@ -45,19 +50,21 @@ class MainActivity : AppCompatActivity() {
             resultLabel?.text = pressedButtonText
         }
         else{
-            resultLabel?.text = resultLabel?.text.toString().plus(pressedButtonText);
+            resultLabel?.text = resultLabel?.text.toString().plus(pressedButtonText)
         }
     }
 
-    fun Delete(view: View){
+    /*
+    * The Delete method is called when the delete (backspace) button is pressed. Removes the last entered character.
+    */
+    fun delete(view: View){
 
         val resultLabelText = resultLabel?.text
 
         //If the length is higher then two, we can safely check if the last input was an operation symbol.
         if (resultLabelText?.length!! > 2){
-
             //Check if the last input is the operation symbol. If it is, remove the last 3 characters.
-            if (operationSymbols.contains(resultLabelText[resultLabelText.count() - 2].toString())) {
+            if (operationSymbols.contains(resultLabelText[resultLabelText.count() - 2].toString()) && resultLabelText[resultLabelText.count() - 1].toString() == " ") {
                 resultLabel?.text = resultLabel?.text?.substring(0, resultLabel!!.text.count() - 2)
             }
         }
@@ -69,33 +76,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun Clear(view: View){
+    /*
+    * The Clear method is called when the clear button is pressed. It resets the history as well as the current entered numbers.
+    */
+    fun clear(view: View){
         resultLabel?.text = "0"
-        historyLabel?.text = "";
+        historyLabel?.text = ""
     }
 
-    fun ChangePolarity(view: View){
+    /*
+    * The ChangePolarity method is called when the plus/minus button is pressed. It reverses the polarity of the last entered number.
+    */
+    fun changePolarity(view: View){
         val equation = resultLabel?.text?.split(" ")?.toMutableList()
 
-        equation?.set(equation.size - 1, (equation[equation.size - 1].toDouble() * -1).toString());
+        if (!operationSymbols.contains(equation?.get(equation.size - 1)))
+            equation?.set(equation.size - 1, (equation[equation.size - 1].toInt() * -1).toString())
 
         resultLabel?.text = equation?.joinToString(" ")
     }
 
-    fun Evaluate(view: View)
+    /*
+    * This is the function which handles all the evaluating of the equation. It is executed when the equal sign is pressed.
+    */
+    fun evaluate(view: View)
     {
-        historyLabel?.text = resultLabel?.text.toString().plus(" =");
+
+        //split the equation into an array of strings
         var equation = resultLabel?.text?.split(" ")?.toMutableList()
-        var result = "";
-        var head: List<String>;
-        var tail: List<String>;
+        var result = ""
+        var head: List<String>
+        var tail: List<String>
+
+        //if the equation isnt a single number, we do the process. Otherwise we return the single number
+
+        if (equation?.size!! > 2) {
+
+            //if user clicked evaluate while the last input was an operator, we remove the last operator from the equation.
+            if (operationSymbols.contains(equation[equation.size - 2]) && equation[equation.size - 1].isEmpty()){
+                equation.removeAt(equation.size - 1)
+                equation.removeAt(equation.size - 1)
+            }
+
+            //set the history label
+            historyLabel?.text = equation.joinToString(" ").plus(" =")
 
 
-        if (equation != null && equation.size != 1) {
+            //while we have multiple operators/numbers, we keep looping to solve them one at a time.
             while (equation!!.size > 1){
-                for ((index, value) in equation!!.withIndex()) {
-                    println("the element at $index is $value")
 
+                //we go through each number/operator in the equation.
+                for ((index, value) in equation!!.withIndex()) {
+
+                    //check for div, multi, modulus
                     if (listOf("÷", "×", "%").contains(equation[index])){
                         when(equation[index]){
                             "÷" -> result = (equation[index - 1].toDouble() / equation[index + 1].toDouble()).toString()
@@ -104,6 +137,7 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
+                    //check for addition, sub if there are no more of the above in the equation
                     else if (listOf("+", "-").contains(equation[index]) && !equation.any { it == "÷" || it == "×" || it == "%" }){
                         when(equation[index]){
                             "+" -> result = (equation[index - 1].toDouble() + equation[index + 1].toDouble()).toString()
@@ -111,6 +145,7 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
+                    //if the index fell on an operator, there would have been a result. we take the result, take apart the equation, insert it where needed, and reset the loop to start over.
                     if (result.isNotEmpty()){
                         head = equation.slice(0 until index - 1)
                         tail = equation.slice(index + 2 until equation.size)
@@ -118,16 +153,13 @@ class MainActivity : AppCompatActivity() {
 
 
                         result = ""
-                        break;
+                        break
                     }
                 }
             }
         }
 
-        else{
-
-        }
-
-        resultLabel?.text = equation?.first();
+        //after all is set and done, set the result label. Round it as well if needed.
+        resultLabel?.text = if (equation.first().toDouble() % 1 == 0.0) equation.first().toDouble().roundToInt().toString() else "%.8f".format(equation.first().toDouble())
     }
 }
